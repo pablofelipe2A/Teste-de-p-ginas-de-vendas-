238921eab82c223b263d8775d39dcc7781ce9fa0
pablofelipe2A <0000110042331xsp@al.educacao.sp.gov.br>
2024-12-09 21:09:39 -0300

    Atualizar o javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-....
+function buyProduct(name, price) {
+    const confirmBuy = confirm(`Você selecionou "${name}" por R$ ${price}. Deseja continuar para o pagamento?`);
+    if (confirmBuy) {
+        const cardNumber = prompt("Insira o número do seu cartão:");
+        if (cardNumber) {
+            alert(`Pagamento confirmado para o produto "${name}"!`);
+        } else {
+            alert("Compra cancelada.");
+        }
+    }
+}
