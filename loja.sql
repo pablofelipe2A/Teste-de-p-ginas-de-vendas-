bd5864e3c71ddca73658f9840172779179c73819
pablofelipe2A <0000110042331xsp@al.educacao.sp.gov.br>
2025-08-01 19:56:47 -0300

    ....

--- loja.sql
+++ loja.sql
@@ -4,6 +4,27 @@ CREATE TABLE produtos (
     preco REAL NOT NULL
 );
 
+CREATE TABLE clientes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL,
+    senha TEXT NOT NULL
+);
+
+CREATE TABLE pedidos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cliente_id INTEGER,
+    data_pedido DATE NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE TABLE pedidos_produtos (
+    pedido_id INTEGER,
+    produto_id INTEGER,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
 CREATE TABLE vendas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     produto_id INTEGER,
@@ -12,10 +33,16 @@ CREATE TABLE vendas (
     FOREIGN KEY (produto_id) REFERENCES produtos(id)
 );
 
--- Exemplos de inserção
+-- Exemplos de dados
 INSERT INTO produtos (nome, preco) VALUES ('Geladeira', 1800);
 INSERT INTO produtos (nome, preco) VALUES ('Televisão 50"', 2200);
 INSERT INTO produtos (nome, preco) VALUES ('Sofá 3 lugares', 1200);
+INSERT INTO produtos (nome, preco) VALUES ('Microondas', 450);
+INSERT INTO produtos (nome, preco) VALUES ('Cama Box', 900);
+
+INSERT INTO clientes (nome, email, senha) VALUES ('João', 'joao@email.com', '123456');
+INSERT INTO clientes (nome, email, senha) VALUES ('Maria', 'maria@email.com', 'senha123');
 
 INSERT INTO vendas (produto_id, quantidade, data_venda) VALUES (1, 2, '2025-08-01');
 INSERT INTO vendas (produto_id, quantidade, data_venda) VALUES (2, 1, '2025-08-01');
+INSERT INTO vendas (produto_id, quantidade, data_venda) VALUES (3, 1, '2025-08-01');
